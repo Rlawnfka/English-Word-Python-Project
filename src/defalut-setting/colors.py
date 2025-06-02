fc7b89867de96899a64fb66200020f3e861afadf
KwakJakyeong <ggyul847@gmail.com>
2025-06-02 12:19:42 +0900

    create files

---
+++ src/defalut-setting/colors.py
@@ -0,0 +1,22 @@
+text = {
+    "primary" : "1F2937",
+    "secondary" : "6B7280",
+}
+
+color = {
+    "primary" : "73A8FF",
+    "secondary" : "CEE5FF",
+    "third" : "E4F3FF",
+    "hover" : "2563EB",
+    "nav" : "93C5FD",
+    "gray" : "B0B0B0",
+}
+
+background = {
+    "main" : "F3FBFF",
+    "card" : "FFFFFF",
+    "card-hover" : "D9D9D9",
+}
+
+print(color["primary"])
+print(background["main"])
--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 # gui entrance
 # create window & event roop
+
+def main():
+    print('temp')
+
+if main == "__init__":
+    main()
