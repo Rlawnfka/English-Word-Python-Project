c3ce15e1e27ade7c3e25ed6f720c055ab7cdfc31
Juram <165885903+Rlawnfka@users.noreply.github.com>
2025-06-05 23:48:14 +0900

    Update colors.py
    색상코드 앞에 # 안 넣으면 색상이 안 들어가서  # 넣어줬습니다.

--- src/defalut-setting/colors.py
+++ src/defalut-setting/colors.py
@@ -1,23 +1,23 @@
 # 딕셔너리 형태
 
 text = {
-    "primary" : "1F2937",
-    "secondary" : "6B7280",
+    "primary" : "#1F2937",
+    "secondary" : "#6B7280",
 }
 
 color = {
-    "primary" : "73A8FF",
-    "secondary" : "CEE5FF",
-    "third" : "E4F3FF",
-    "hover" : "2563EB",
-    "nav" : "93C5FD",
-    "gray" : "B0B0B0",
+    "primary" : "#73A8FF",
+    "secondary" : "#CEE5FF",
+    "third" : "#E4F3FF",
+    "hover" : "#2563EB",
+    "nav" : "#93C5FD",
+    "gray" : "#B0B0B0",
 }
 
 background = {
-    "main" : "F3FBFF",
-    "card" : "FFFFFF",
-    "card-hover" : "D9D9D9",
+    "main" : "#F3FBFF",
+    "card" : "#FFFFFF",
+    "card-hover" : "#D9D9D9",
 }
 
 # 사용법
