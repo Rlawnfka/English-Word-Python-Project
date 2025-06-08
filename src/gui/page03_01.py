fc246e75152c15decfd3f108099b14816918895e
KwakJakyeong <ggyul847@gmail.com>
2025-06-09 07:37:49 +0900

    create files 상세 단어페이지

---
+++ src/gui/page03_01.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtCore import *
+
+from gui.DefaultLayout import DefaultLayout
+
+### NOTICE! : wordList 상세 페이지 ->  word 복습
+
+class page03_01(DefaultLayout):
+     def __init__(self):
+          super().__init__()
