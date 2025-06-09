610d4bf7e6f63a37714371579677018caa2ad427
Rlawnfka <gimjulam2@gmail.com>
2025-06-09 23:33:03 +0900

    Add DB data(firstDate, language), Add editButton, Add binButton

--- src/gui/page03.py
+++ src/gui/page03.py
@@ -4,6 +4,9 @@ from PyQt6.QtCore import *
 
 from gui.DefaultLayout import DefaultLayout
 
+from ..data import word_manager as DATA
+from data.word_manager import *
+
 ### NOTICE! : wordList 상세 페이지 & word 복습
 
 class page03(DefaultLayout):
@@ -12,24 +15,53 @@ class page03(DefaultLayout):
           self.contentLayout.addWidget(categoryInfo())
           self.contentLayout.addWidget(unknownWordButtons())
           
-
-
 class categoryInfo:
      def __init__(self):
           super().__init__()
-          categoryTitle = QLabel("unit4 English")
-          date = QLabel("2024/05/25"+" · "+"English") #날짜와 언어는 db에서 가져오기
 
-class unknownWordButtons: # 버튼 좌우 수평 레이아웃
+          
+          # db에서 가져온 정보들
+          language = DATA.doc["language"] 
+          firstDate = DATA.onlyDate
+
+          categoryTitle = QLabel("unit4 English")
+          date = QLabel(firstDate+" · "+language) #날짜와 언어는 db에서 가져오기
+
+          self.setWidget(categoryTitle)
+          self.setWidget(date)
+
+class editRemoveButtons:
      def __init__(self):
           super().__init__()
-          button_layout = QHBoxLayout() 
+
+          layout = QHBoxLayout()
+
+          self.editButton = QPushButton()
+          self.editButton.setIcon(QIcon("/src/assets/icons/editIcon.png"))
+          self.editButton.setIconSize(32,32) 
+          self.editButton.setFixedSize(32,32) # 창이 늘어나도 크기 변하지 않음
+          self.editButton.clicked.connect() # page03_01.py로 이동
+
+          self.binButton = QPushButton()
+          self.binButton.setIcon(QIcon("/src/assets/icons/iconBin.png"))
+          self.binButton.setIconSize(32,32)
+          self.binButton.setFixedSize(32,32)
+          self.binButton.clicked.connect()
+
+          layout.addStretch() # 오른쪽 정렬
+          layout.addWidget(self.editButton)
+          layout.addWidget(self.binButton)
+
+class unknownWordButtons: 
+     def __init__(self):
+          super().__init__()
+          layout = QHBoxLayout() 
           reviewButton = QPushButton("REVIEW")
           reviewButton.clicked.connect()
           staredButton = QPushButton("STARED")
           staredButton.clicked.connect()
 
-          button_layout.addStretch() # 오른쪽 정렬
+          layout.addStretch() # 오른쪽 정렬
           self.addWidget(reviewButton)
           self.addWidget(staredButton)
 
