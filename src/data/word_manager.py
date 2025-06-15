5b1562c656e877ec7350d8cfc7a2dbcae20f7f0d
Rlawnfka <gimjulam2@gmail.com>
2025-06-15 13:10:51 +0900

    - Add connectDB class   - Add exception handling    - Add close client

--- src/data/word_manager.py
+++ src/data/word_manager.py
@@ -4,11 +4,22 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-class connectDB:
-    # .env 파일의 환경 변수들 불러오기
-    load_dotenv()
+# .env 파일의 환경 변수들 불러오기
+load_dotenv()
 
-    # mongodb 비밀번호 가져오기
-    MONGO_URL = os.getenv("MONGO_URL")
-    client = MongoClient(MONGO_URL)
-    db = client["Titles"]
+
+class connectDB:
+    def __init__(self):
+
+        # mongodb 비밀번호 가져오기
+        MONGO_URL = os.getenv("MONGO_URL")
+
+        if not MONGO_URL:
+            raise ValueError("환경변수 mongo_url이 설정되지 않음")
+        
+        self.client = MongoClient(MONGO_URL)
+        self.db = self.client["Titles"] # 외부에서 접근 가능 (self.db)
+        
+    
+    def close(self):
+        self.client.close()
