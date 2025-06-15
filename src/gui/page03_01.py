cf2c9486fc3189ffc3683e8b23b403464a0b834f
Rlawnfka <gimjulam2@gmail.com>
2025-06-15 13:44:53 +0900

    Change inherite

--- src/gui/page03_01.py
+++ src/gui/page03_01.py
@@ -6,6 +6,6 @@ from gui.DefaultLayout import DefaultLayout
 
 ### NOTICE! : wordList 상세 페이지 ->  word 복습
 
-class page03_01(DefaultLayout):
+class page03_01(QWidget):
      def __init__(self):
           super().__init__()
