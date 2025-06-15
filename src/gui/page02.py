4860027f9bfdf66053da519a4ddd5ee7b5bf935a
Rlawnfka <gimjulam2@gmail.com>
2025-06-15 14:53:22 +0900

    Add scroll(not yet)

--- src/gui/page02.py
+++ src/gui/page02.py
@@ -10,14 +10,31 @@ from defalut_setting.colors import *
 class page02(QWidget):
      def __init__(self):
           super().__init__()
-          self.contentLayout = QVBoxLayout()
-          self.setLayout(self.contentLayout)
+          # 전체 레이아웃
+          mainLayout = QVBoxLayout(self)
+          
+          # 스크롤 영역
+          scroll = QScrollArea()
+          scroll.setWidgetResizable(True)
+
+          # 스크롤 내부에 넣을 콘텐츠 위젯 생성
+          scrollContent = QWidget()
+          self.contentLayout = QVBoxLayout(scrollContent)
 
           # DB에서 삽입
           for i in range(4):
                info = createShowInfo("unit English", "2024/05/24", "English")
                self.contentLayout.addWidget(info)
+
+          # 플러스 버튼 추가
           self.contentLayout.addWidget(info.createPlusBtn())
+
+
+          # 콘텐츠를 스크롤 영역에 넣기
+          scroll.setWidget(scrollContent)
+
+          # 메인에 스크롤 추가
+          mainLayout.addWidget(scroll)
 
 
 class createShowInfo(QWidget):
