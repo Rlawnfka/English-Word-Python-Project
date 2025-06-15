f2b03068b727292a9e4cf16f3a3cedff59a6216d
Rlawnfka <gimjulam2@gmail.com>
2025-06-15 13:06:26 +0900

    - Change layout QBoxLayout > QHBoxLayout  - Add layout(logo)  - Change icons path   - Add setLayout(layout)

--- src/gui/DefaultLayout.py
+++ src/gui/DefaultLayout.py
@@ -1,75 +1,99 @@
 import sys
+import os
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 
 from defalut_setting.colors import *
 
-# XXX : stackedLayout 연결이 안됌. 특히 nav와의 연결이 불완전
-# nav를 DefaultLayout의 함수로 바꾸면서 해결중
+
 class DefaultLayout(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.HEIGHT = 900
+        self.HEIGHT = 790
         self.WIDTH = 550
-        
-        # 기본 세팅, 움직임
-        # 전에 열렸던 자리 기억
+
+        self.setWindowTitle("Shorty!")
+        self.setFixedSize(self.WIDTH, self.HEIGHT)
+
         self.setStyleSheet(f"""
             *{{
                 background-color: {BACKGROUND['main']};
             }}
         """)
-        
-        self.setWindowTitle("Shorty!")
-        self.setFixedSize(self.WIDTH, self.HEIGHT)
-        
+
+
+        # 전체 레이아웃
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        # 상단에 Logo 고정
-        self.layout.addWidget(CreateLogo())
+        # ----- 상단 로고
+        self.logoWidget = CreateLogo()
+        self.layout.addWidget(self.logoWidget)
 
-        # 중간 내용 <- 각 페이지의 클래스에서 설정 (상속 통해서)
-        self.contentLayout = QVBoxLayout()
+        # ----- 중단 컨텐츠 영역 (Stacked Layout)
         self.stack = QStackedLayout()
+        self.contentWidget = QWidget()
+        self.contentWidget.setLayout(self.stack)
+        self.layout.addWidget(self.contentWidget)
 
-        self.contentLayout.addLayout(self.stack)
-        
-        self.layout.addLayout(self.contentLayout)
+        # ----- 하단 Nav (초기값으로 빈 위젯, 나중에 교체)
+        self.navWidget = QWidget()
+        self.layout.addWidget(self.navWidget)
 
-        # 하단 nav 고정
-        self.layout.addWidget(CreateNav(self.stack))
-        
+    def addPage(self, widget):
+        """Stack에 페이지 추가"""
+        self.stack.addWidget(widget)
+
+    def setContent(self, index: int):
+        """현재 보여줄 페이지 설정"""
+        self.stack.setCurrentIndex(index)
+
+    def setNav(self, navWidget):
+        """하단 네비게이션을 교체"""
+        self.layout.replaceWidget(self.navWidget, navWidget)
+        self.navWidget.deleteLater()
+        self.navWidget = navWidget
+        self.layout.addWidget(self.navWidget)
+
 
 class CreateLogo(QWidget):
     def __init__(self):
         super().__init__()
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+
         logo = QLabel("Shorty!", self)
         logo.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignTop)
 
-        font = QFont("Tilt Warp",32,QFont.Weight.Normal)
+        font = QFont("Tilt Warp", 32, QFont.Weight.Normal)
         logo.setFont(font)
 
         logo.setStyleSheet("""
             color: #000000;
-            font-size : 32px;
+            font-size: 32px;
             font-weight: 400;
             font-style: normal;
         """)
-        
+        layout.addWidget(logo)
+
 
 class CreateNav(QWidget):
-    def __init__(self, stack: QStackedLayout):      # 스택을 받아서 사용 DefaultLayout -> CreateNav
+    def __init__(self, stack: QStackedLayout):
         super().__init__()
         self.stack = stack
 
-        iconHome = QIcon("../assets/icons/iconHome.svg")
-        iconAdd = QIcon("../assets/icons/iconAdd.svg")
-        iconSetting = QIcon("../assets/icons/iconSetting.svg")
-        iconProfile = QIcon("../assets/icons/iconProfile.svg")
-        
+        # 상대 경로 -> 절대 경로로 변환
+        homeIcon_path = os.path.join(os.path.dirname(__file__), "../assets/icons/iconHome.svg")
+        addIcon_path = os.path.join(os.path.dirname(__file__), "../assets/icons/iconAdd.svg")
+        settingIcon_path = os.path.join(os.path.dirname(__file__), "../assets/icons/iconSetting.svg")
+        profileIcon_path = os.path.join(os.path.dirname(__file__), "../assets/icons/iconProfile.svg")
+        iconHome = QIcon(homeIcon_path)
+        iconAdd = QIcon(addIcon_path)
+        iconSetting = QIcon(settingIcon_path)
+        iconProfile = QIcon(profileIcon_path)
+
         iconList = [iconHome, iconAdd, iconSetting, iconProfile]
         page = [0, 3, 1, 2]
 
@@ -78,19 +102,19 @@ class CreateNav(QWidget):
         for i, icon in enumerate(iconList):
             button = QPushButton()
             button.setIcon(icon)
-            button.setIconSize(QSize(35,35))
-            button.setFlat(True) # 입체감 없애기
-            button.setCheckable(True) # 클릭 상태 유지  isChecked()로 확인 후 hover먹이기
-            button.clicked.connect(lambda _, index=i: self.stack.setCurrentIndex(page[i])) # 인덱스 끝으로 인식되는 문제 -> index=i로 해결
+            button.setIconSize(QSize(35, 35))
+            button.setFlat(True)
+            button.setCheckable(True)
+            button.clicked.connect(lambda _, index=i: self.stack.setCurrentIndex(page[index]))
             button.setStyleSheet(f"""
                 QPushButton{{
                     color: {TEXT['primary']};
-                    background-color : transparent;
+                    background-color: transparent;
                 }}
                 QPushButton:hover{{
                     color: {COLOR['hover']};
                 }}
             """)
             layout.addWidget(button)
-    
-        
+
+        self.setLayout(layout)
